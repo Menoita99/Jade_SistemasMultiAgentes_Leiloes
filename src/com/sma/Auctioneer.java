9ddffc59ee1d802746146f210d1ec2e5971bf1ab
Francisco Cardoso <60990445+Franciscodc98@users.noreply.github.com>
2021-01-27 23:38:17 +0000

    Accept done

--- src/com/sma/Auctioneer.java
+++ src/com/sma/Auctioneer.java
@@ -9,6 +9,7 @@ import jade.domain.DFService;
 import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,40 +17,42 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
-public class Auctioneer extends Agent{
-	
+public class Auctioneer extends Agent {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private Auction auction;
 	private AuctioneerBehavior behavior = new AuctioneerBehavior();
 	private LinkedList<AID> bidders = new LinkedList<>();
-	
+
 	public Auctioneer() {
 		auction = new Auction();
 	}
-	
+
 	@Override
 	protected void setup() {
 		try {
-			searchForBidders();
 			regist();
+			searchForBidders();
+			addBehaviour(behavior);
+
 		} catch (FIPAException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private void regist() throws FIPAException {
-		addBehaviour(behavior);
+
 		DFAgentDescription dfd = new DFAgentDescription();
 		ServiceDescription sd = new ServiceDescription();
 		dfd.setName(getAID());
 		sd.setType("auctionner");
-		sd.setName(getLocalName()+" auctioneer");
+		sd.setName(getLocalName() + " auctioneer");
 		dfd.addServices(sd);
 		DFService.register(this, dfd);
-		System.out.println("Auctionner "+getName()+" with aid "+getAID()+" ready");
+		System.out.println("Auctionner " + getName() + " with aid " + getAID() + " ready");
 	}
-	
+
 	protected void searchForBidders() {
 		DFAgentDescription template = new DFAgentDescription();
 		ServiceDescription sd = new ServiceDescription();
@@ -60,22 +63,64 @@ public class Auctioneer extends Agent{
 			bidders.clear();
 			for (int i = 0; i < result.length; ++i)
 				bidders.add(result[i].getName());
+		} catch (FIPAException fe) {
+			fe.printStackTrace();
 		}
-		catch (FIPAException fe) { fe.printStackTrace(); }
-	} 
+	}
 
-	
-	
-	
-	
-	private class AuctioneerBehavior extends CyclicBehaviour{
+	private class AuctioneerBehavior extends CyclicBehaviour {
 
 		private static final long serialVersionUID = 1L;
 
 		@Override
 		public void action() {
+			ACLMessage msg = myAgent.receive();
+			MessageType type = MessageType.valueOf(msg.getContent().split("\n")[0]);
+			switch (type) {
+			case JOIN: {
+				processJoin(msg);
+				break;
+			}
+			case BIDDING: {
+				processBidding(msg);
+				break;
+			}
+			case PRIORITIES: {
+				processPriorities(msg);
+				break;
+			}
+			case WINNER: {
+				// processWinner(msg);
+				break;
+			}
+			default:
+				throw new IllegalArgumentException("Unexpected value: " + type);
+			}
+
+		}
+
+		private void processPriorities(ACLMessage msg) {
 			// TODO Auto-generated method stub
-			
+
+		}
+
+		private void processBidding(ACLMessage msg) {
+			// TODO Auto-generated method stub
+
+		}
+
+		private void processJoin(ACLMessage msg) {
+			ACLMessage response = new ACLMessage(ACLMessage.INFORM);
+			if (auction.getFase() == AuctionFase.Open) {
+				if (msg.getPerformative() == ACLMessage.REQUEST) {
+					bidders.add(msg.getSender());
+					response.setContent(MessageType.ACCEPT.toString() + "\n");
+					response.addReceiver(msg.getSender());
+					System.out.println("Auctioneer sent ACCEPT to :" + msg.getSender());
+				}
+			} else if (msg.getPerformative() == ACLMessage.REFUSE) {
+				bidders.remove(msg.getSender());
+			}
 		}
 	}
 }
